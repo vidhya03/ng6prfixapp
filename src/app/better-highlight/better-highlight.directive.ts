3207eb6d4e8180cd42bd45de3c7cbd659ede5467
Vidhyadharan D <it.vidhyadharan@gmail.com>
2018-07-03 09:08:46 +0530

    Hostbinding for style change - better way todo html property update dynamically

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,20 +1,29 @@
-import { Directive, OnInit, Renderer2, ElementRef, RendererStyleFlags2, HostListener } from "@angular/core";
+import { Directive,
+   OnInit,
+   Renderer2,
+   ElementRef,
+   RendererStyleFlags2,
+   HostListener,
+   HostBinding } from "@angular/core";
 
 @Directive({
   selector: "[appBetterHighlight]"
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
 
   }
   @HostListener('mouseenter') mouseenter(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
+    // this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
+    this.backgroundColor = 'blue';
     this.renderer.setStyle(this.elRef.nativeElement,'color','yellow',RendererStyleFlags2.DashCase);
   }
   @HostListener('mouseleave') mouseleave(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement,'background-color','transparent');
+    // this.renderer.setStyle(this.elRef.nativeElement,'background-color','transparent');
+    this.backgroundColor = 'transparent';
     this.renderer.setStyle(this.elRef.nativeElement,'color','black');
   }
 }
